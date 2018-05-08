82343ec0e697b35e0c045ec9b1e6b071d9ed82da
Mikołaj Pich <m.pich@outlook.com>
2018-05-08 17:14:30 +0200

    Add google play download button

---
+++ app.js
@@ -0,0 +1,40 @@
+var disableClick = function(e) {e.preventDefault()};
+
+var addButtonInfo = function(url, button, version, cb) {
+    var buttonElement = document.querySelector(button);
+    buttonElement.addEventListener("click", disableClick);
+    var versionElement = document.querySelector(version);
+    versionElement.innerHTML = "Ładowanie…";
+
+    var res = new XMLHttpRequest();
+    res.open("GET", url, true);
+    res.onloadend = function () {
+        if(res.status === 200) {
+            cb(buttonElement, versionElement, res);
+        } else {
+            versionElement.innerHTML = "wersja nieznana";
+        }
+        buttonElement.removeEventListener("click", disableClick);
+    };
+    res.send(null);
+};
+
+addButtonInfo("https://api.github.com/repos/wulkanowy/wulkanowy/releases/latest", "#alfa-button", "#alfa-version",
+    function(button, version, res) {
+        var json = JSON.parse(res.responseText);
+        button.href = json.assets[0].browser_download_url;
+        version.innerHTML = "v" + json.name;
+    }
+);
+
+addButtonInfo("https://bitrise-redirector.herokuapp.com/v0.1/apps/daeff1893f3c8128/builds/master/artifacts/app-debug-bitrise-signed.apk/info", "#nightly-button", "#nightly-version",
+    function(button, version, res) {
+        var json = JSON.parse(res.responseText);
+        button.href = json.expiring_download_url;
+        version.innerHTML = "v" + json.build_number;
+
+        setTimeout(function() {
+            button.href = json.public_install_page_url;
+        }, 60 * 1000);
+    }
+);
