8b9ee51b0d1d039c90fc9b3b5caccdc174934753
Dominik Korsa <dominik.korsa@gmail.com>
2019-02-23 09:54:32 +0100

    Create HomeView

---
+++ src/components/HomeMainSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home-main-section">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-main-section',
+  components: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  #home-main-section {
+    width: 100vw;
+    height: 100vh;
+    background:
+      linear-gradient(
+        rgba(0, 0, 0, 0.4),
+        rgba(0, 0, 0, 0.4)
+      ),
+      url("../assets/wallpaper.jpg");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    overflow: auto;
+  }
+</style>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    <home-main-section />
+  </div>
+</template>
+
+<script>
+import HomeMainSection from './HomeMainSection.vue';
+
+export default {
+  name: 'home-view',
+  components: {
+    HomeMainSection,
+  },
+};
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+  }
+</style>
