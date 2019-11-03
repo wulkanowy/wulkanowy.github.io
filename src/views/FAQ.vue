d694f9d1c81af8fadb62729faad4bdeef687dbea
Dominik Korsa <dominik.korsa@gmail.com>
2019-11-03 22:23:38 +0100

    Add answer font

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -219,5 +219,10 @@
       0px 2px 4px -1px rgba(0, 0, 0, 0.2),
       0px 4px 5px 0px rgba(0, 0, 0, 0.14),
       0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+    font-family: 'Roboto', sans-serif;
+
+    h1:first-child {
+      margin-top: 8px;
+    }
   }
 </style>
