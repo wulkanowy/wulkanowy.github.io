40aa5614f6ee2c9b19c343daee85962cc7699758
Daxxxis <czupryn845@gmail.com>
2020-12-27 21:55:37 +0100

    Remove email quick link

--- src/components/HomeMainHeader.vue
+++ src/components/HomeMainHeader.vue
@@ -31,9 +31,6 @@
       </button>
     </div>
     <div class="quick-links">
-      <a class="email" title="Email" href="mailto:wulkanowyinc@gmail.com" @click="emailClick">
-        <img src="../assets/email.svg" alt="Email" />
-      </a>
       <a class="discord" title="Discord" href="https://discord.gg/vccAQBr" @click="discordClick">
         <img src="../assets/discord.svg" alt="Discord" />
       </a>
@@ -77,12 +74,6 @@
           });
           element.focus();
         }
-      },
-      emailClick() {
-        this.$ga.event({
-          eventCategory: 'email-link',
-          eventAction: 'click',
-        });
       },
       facebookClick() {
         this.$ga.event({
@@ -234,7 +225,7 @@
   .quick-links {
     margin-top: 32px;
 
-    .email, .github, .discord, .paypal, .facebook {
+    .github, .discord, .paypal, .facebook {
       margin: 8px;
 
       img {
