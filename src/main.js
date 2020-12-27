11398554f04022f560c9f6535a4ff403168eec3f
Daxxxis <czupryn845@gmail.com>
2020-12-27 22:07:44 +0100

    Fix

--- src/main.js
+++ src/main.js
@@ -8,26 +8,26 @@ import router from './router';
 
 Vue.use(VueResource);
 Vue.use(VueMq, {
-  breakpoints: {
-    md: 875,
-    lg: Infinity,
-  },
+    breakpoints: {
+        md: 875,
+        lg: Infinity,
+    },
 });
 Vue.use(VueAnalytics, {
-  id: 'UA-99646499-3',
-  debug: {
-    enabled: window.location.hostname === 'localhost',
-    sendHitTask: window.location.hostname !== 'localhost',
-  },
+    id: 'UA-99646499-3',
+    debug: {
+        enabled: window.location.hostname === 'localhost',
+        sendHitTask: window.location.hostname !== 'localhost',
+    },
 });
 Vue.$ga.page(window.location.pathname);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: (h) => h(App),
-  mounted() {
-    document.dispatchEvent(new Event('x-app-rendered'));
-  },
+    router,
+    render: (h) => h(App),
+    mounted() {
+        document.dispatchEvent(new Event('x-app-rendered'));
+    },
 }).$mount('#app');
